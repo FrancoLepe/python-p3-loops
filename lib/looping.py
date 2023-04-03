b15872eaaf43a1f7037c89bb85d61babba11675c
Franco Lepe <Francolepe169@gmail.com>
2023-04-02 21:06:14 -0400

    done

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,21 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    pass
+    for i in range(10, 0, -1):
+        print(i)
+
+    print("Happy New Year!")
 
 def square_integers(int_list):
-    # code goes here!
-    pass
+   return [i_number ** 2 for i_number in int_list]
 
 def fizzbuzz():
-    # code goes here!
-    pass
+    for numbers in range(1, 101):
+        if not numbers % 15:
+            print("FizzBuzz")
+        elif not numbers % 5:
+            print("Buzz")
+        elif not numbers % 3:
+            print("Fizz")
+        else:
+            print(numbers)
